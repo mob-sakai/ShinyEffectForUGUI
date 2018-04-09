7c6b9d248f43d94387b5d9fcc68386d183c05035
Takashi Sakai <sakai@mobcast.jp>
2018-04-09 21:36:32 +0900

    Implement shiny effect

---
+++ Assets/ShinyEffectForUGUI/Demo/Demo_ShinyEffectForUGUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Coffee.UIExtensions
+{
+	public class Demo_ShinyEffectForUGUI : MonoBehaviour
+	{
+		public void SetTimeScale(float scale)
+		{
+			Time.timeScale = scale;
+		}
+	}
+}
